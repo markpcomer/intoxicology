bd9d232ae0fa12ee043ad8a0b4d4101cafeabf03
markpcomer <markpcomer@gmail.com>
2022-02-10 18:21:19 -0500

    cocktail sorted files

---
+++ cocktailsSORTABLE.js
@@ -0,0 +1,348 @@
+let cocktailsArray = [
+{	name: "Aperol Spritz",
+	image: "Images/waldorf.jpg",
+	liqueur: "Aperol",
+	syrup: " ",
+	juice: " ",
+	soda: ["Prosecco", "Club Soda"],
+	bitters: " ",
+	'egg white': " "
+},
+{	name: "Aviation",
+	image: "Images/waldorf.jpg",
+	spirit: "Gin",
+	liqueur: ["Marashino", "Creme de Violette"],
+	syrup: " ",
+	juice: "Lemon Juice",
+	soda: " ",
+	bitters: " ",
+	'egg white': " "
+},
+{	name: "Boulevardier",
+	image: "Images/waldorf.jpg",
+	spirit: "Whiskey",
+	liqueur: ["Campari", "Sweet Vermouth"],
+	syrup: " ",
+	juice: " ",
+	soda: " ",
+	bitters: " ",
+	'egg white': " "
+},
+{	name: "The Brooklyn",
+	image: "Images/waldorf.jpg",
+	spirit: "Whiskey",
+	liqueur: ["Dry Vermouth", "Marashino", "Amer Picon"],
+	syrup: " ",
+	juice: " ",
+	soda: " ",
+	bitters: " ",
+	'egg white': " "
+},
+{	name: "Cosmopolitan",
+	image: "Images/waldorf.jpg",
+	spirit: "Vodka",
+	liqueur: "Triple Sec",
+	syrup: " ",
+	juice: ["Lime Juice", "Cranberry Juice"],
+	soda: " ",
+	bitters: " ",
+	'egg white': " "
+},
+{	name: "Cuba Libre",
+	image: "Images/waldorf.jpg",
+	spirit: "Rum",
+	liqueur: " ",
+	syrup: " ",
+	juice: " ",
+	soda: "Cola",
+	bitters: " ",
+	'egg white': " "
+},
+{	name: "Daiquiri",
+	image: "Images/waldorf.jpg",
+	spirit: "Rum",
+	liqueur: " ",
+	syrup: "Agave",
+	juice: "Lime Juice",
+	soda: " ",
+	bitters: " ",
+	'egg white': " "
+},
+{	name: "Espresso Martini",
+	image: "Images/waldorf.jpg",
+	spirit: "Vodka",
+	liqueur: "Coffee Liqueur",
+	syrup: " ",
+	juice: "Espresso",
+	soda: " ",
+	bitters: " ",
+	'egg white': " "
+},
+{	name: "French 75",
+	image: "Images/waldorf.jpg",
+	spirit: "Gin",
+	liqueur: " ",
+	syrup: "Simple Syrup",
+	juice: "Lemon Juice",
+	soda: " ",
+	bitters: " ",
+	'egg white': " "
+},
+{	name: "Gimlet",
+	image: "Images/waldorf.jpg",
+	spirit: "Gin",
+	liqueur: " ",
+	syrup: "Simple Syrup",
+	juice: "Lemon Juice",
+	soda: " ",
+	bitters: " ",
+	'egg white': " "
+},
+{	name: "The Great Pumpkin",
+	image: "Images/waldorf.jpg",
+	spirit: "Whiskey",
+	liqueur: ["Apple Brandy"],
+	syrup: "Maple Syrup",
+	juice: " ",
+	soda: "Pumpkin Ale",
+	bitters: " ",
+	'egg white': "Yes"
+},
+{	name: "The Longshoreman",
+	image: "Images/waldorf.jpg",
+	spirit: "Whiskey",
+	liqueur: ["Averna Amaro", "Punt e Mes"],
+	syrup: " ",
+	juice: " ",
+	soda: " ",
+	bitters: " ",
+	'egg white': " "
+},
+{	name: "Manhattan",
+	image: "Images/waldorf.jpg",
+	spirit: "Whiskey",
+	liqueur: "Sweet Vermouth",
+	syrup: " ",
+	juice: " ",
+	soda: " ",
+	bitters: "Angostura",
+	'egg white': " "
+},
+{	name: "Margarita",
+	image: "Images/waldorf.jpg",
+	spirit: "Tequila",
+	liqueur: "Triple Sec",
+	syrup: "Simple Syrup",
+	juice: "Lime Juice",
+	soda: " ",
+	bitters: " ",
+	'egg white': " "
+},
+{	name: "Martini",
+	image: "Images/waldorf.jpg",
+	spirit: "Vodka",
+	liqueur: "Dry Vermouth",
+	syrup: " ",
+	juice: " ",
+	soda: " ",
+	bitters: " ",
+	'egg white': " "
+},
+{
+	name: "Mojito",
+	image: "Images/waldorf.jpg",
+	spirit: "Rum",
+	liqueur: " ",
+	syrup: "Simple Syrup",
+	juice: "Lime Juice",
+	soda: " ",
+	bitters: " ",
+	'egg white': " "
+},
+{	name: "Moscow Mule",
+	image: "Images/waldorf.jpg",
+	spirit: "Vodka",
+	liqueur: " ",
+	syrup: "Simple Syrup",
+	juice: "Lime Juice",
+	soda: "Ginger Beer",
+	bitters: " ",
+	'egg white': " "
+},
+{	name: "Negroni",
+	image: "Images/waldorf.jpg",
+	spirit: "Gin",
+	liqueur: ["Sweet Vermouth", "Campari"],
+	syrup: " ",
+	juice: " ",
+	soda: " ",
+	bitters: " ",
+	'egg white': " "
+},
+{	name: "Old Fashioned",
+	image: "Images/waldorf.jpg",
+	spirit: "Whiskey",
+	liqueur: " ",
+	syrup: "Demarura Syrup",
+	juice: " ",
+	soda: " ",
+	bitters: ["Angostura", "Orange"],
+	'egg white': " "
+},
+{	name: "Paloma",
+	image: "Images/waldorf.jpg",
+	spirit: "Tequila",
+	liqueur: " ",
+	syrup: " ",
+	juice: "Lime Juice",
+	soda: "Grapefruit Soda",
+	bitters: " ",
+	'egg white': " "
+},
+{	name: "Ring the Alarm",
+	image: "Images/waldorf.jpg",
+	spirit: "Tequila",
+	liqueur: "Aperol",
+	syrup: "Agave",
+	juice: ["Lime Juice", "Pineapple Juice"],
+	soda: " ",
+	bitters: "Peychaud",
+	'egg white': " "
+},
+{	name: "Sazerac",
+	image: "Images/waldorf.jpg",
+	spirit: "Cognac",
+	liqueur: "Absinthe",
+	syrup: " ",
+	juice: " ",
+	soda: " ",
+	bitters: "Peychauds",
+	'egg white': " "
+},
+{	name: "Sidecar",
+	image: "Images/waldorf.jpg",
+	spirit: "Brandy",
+	liqueur: "Triple Sec",
+	syrup: " ",
+	juice: "Lemon Juice",
+	soda: " ",
+	bitters: " ",
+	'egg white': " "
+},
+{	name: "Tom Collins",
+	image: "Images/waldorf.jpg",
+	spirit: "Gin",
+	liqueur: " ",
+	syrup: "Simple Syrup",
+	juice: "Lemon Juice",
+	soda: "Club Soda",
+	bitters: " ",
+	'egg white': " "
+},
+{	name: "The Waldorf",
+	image: "Images/waldorf.jpg",
+	spirit: "Whiskey",
+	liqueur: ["Absinthe", "Sweet Vermouth"],
+	syrup: " ",
+	juice: " ",
+	soda: " ",
+	bitters: "Angostura",
+	'egg white': " "
+},
+{	name: "Whiskey Sour",
+	image: "Images/waldorf.jpg",
+	spirit: "Whiskey",
+	liqueur: " ",
+	syrup: "Simple",
+	juice: "Lime",
+	soda: " ",
+	bitters: " ",
+	'egg white': "Yes"
+},
+];
+
+
+
+
+function cocktailCardsFunc (drink) {
+	return `<div class="test-card">
+			<div class="test-card-front">
+			</div>
+			<div>
+				<img class="test-image" src=` + drink.image + `>
+			</div>
+			<div class="test-card-reveal fadeIn-bottom">
+				<h3 class="test-cocktail-name">` + drink.name + `</h3>
+				<ul class="test-cocktail-recipe">
+					<li>` + drink.spirit +` </li>
+					<li>` + drink.liqueur +` </li>
+					<li>` + drink.syrup +` </li>
+					<li>` + drink.juice +` </li>
+					<li>` + drink.bitters +` </li>
+				</ul>
+			</div>
+		</div>`
+}
+
+
+function renderCards(arr) {
+	let cocktailCards =  arr.map(drink => cocktailCardsFunc(drink));
+	document.getElementsByClassName('test-container')[0].innerHTML = cocktailCards;
+	console.log('test-card');
+} 
+/* 
+parameters => expression
+
+// is equivalent to:
+
+function (parameters){
+  return expression;
+}
+*/
+
+/*
+function renderCards(array) {
+	let cocktailCards = array.map()
+
+	}
+	
+	) 
+	document.getElementByClassName('test-container')[0].innerHTML = cocktailCards;
+};
+*/
+
+
+
+/*	let add = function (x, y) {
+		return x + y;
+	};  
+
+	let add = (x,y) => {return x +y};
+
+function renderCards(arr) {
+	let cocktailCards = arr.map(function(drink) {
+		return cocktailCardsFunc(drink);
+	});
+	document.getElementByClassName('test-container')[0].innerHTML = cocktailCards;
+};
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
